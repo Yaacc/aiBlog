65de70b69b2686399ecc8179efd20eeaf395d069
yang'jia'cai <2429638614@qq.com>
2023-08-05 16:35:50 +0800

    导入/导出excel文件功能

--- src/main/java/cn/ndky/controller/AdminController.java
+++ src/main/java/cn/ndky/controller/AdminController.java
@@ -1,6 +1,9 @@
 package cn.ndky.controller;
 
 
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import cn.ndky.common.Constants;
 import cn.ndky.config.Result;
 import cn.ndky.entity.Admin;
@@ -12,8 +15,16 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -106,5 +117,51 @@ public class AdminController{
         log.info("将被删除的id：{}", id);
         adminService.removeById(id);
         return Result.success();
+    }
+
+    //导入功能
+    @PostMapping("/import")
+    public Result<?> importExcel(MultipartFile multipartFile) throws Exception{
+        InputStream inputStream = multipartFile.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        //reader.addHeaderAlias("序号","id");
+        reader.addHeaderAlias("管理员编号","adminNumber");
+        reader.addHeaderAlias("用户名","username");
+        reader.addHeaderAlias("密码","password");
+        reader.addHeaderAlias("姓名","realName");
+        reader.addHeaderAlias("性别","sex");
+        reader.addHeaderAlias("年龄","age");
+        reader.addHeaderAlias("身份证号","IDCard");
+        List<Admin> list = reader.readAll(Admin.class);
+        System.out.println(list);
+        boolean saveBatch = adminService.saveBatch(list);
+        if(!saveBatch){
+            return Result.error(Constants.CODE_400,"导入失败,查看用户账号是否重复");
+        }
+        return Result.success("导入成功");
+    }
+    //导出功能
+    @GetMapping("/export")
+    public void exportExcel(HttpServletResponse response)throws Exception {
+        List<Admin> list =adminService.list();
+        ExcelWriter writer= ExcelUtil.getWriter(true);
+        //只导出含有别名的属性
+        writer.setOnlyAlias(true);
+        //writer.addHeaderAlias("id","序号");
+        writer.addHeaderAlias("adminNumber","管理员编号");
+        writer.addHeaderAlias("username","用户名");
+        writer.addHeaderAlias("password","密码");
+        writer.addHeaderAlias("realName","姓名");
+        writer.addHeaderAlias("sex","性别");
+        writer.addHeaderAlias("age","年龄");
+        writer.addHeaderAlias("IDCard","身份证号");
+        writer.write(list, true);
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName= URLEncoder.encode("管理员信息","UTF-8");
+        response.setHeader("Content-Disposition","attachment;filename="+fileName+".xlsx");
+        ServletOutputStream outputStream=response.getOutputStream();
+        writer.flush(outputStream,true);
+        outputStream.close();
+        writer.close();
     }
 }
